e542afac409831a03de8388d020bbcab25241d42
zaheer310 <Administrator@DC1.melikshah.com>
2026-06-25 14:38:36 +0530

    Practice in JavaScript Day_13

--- Day_13/script.js
+++ Day_13/script.js
@@ -198,15 +198,208 @@
 //     console.log(message);
 //   });
 
+// // FInally method(Bonus)
+// // Example :
+// let loadingSpinner = true;
+// function fetchData() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(`User Data received`);
+//     }, 1000);
+//   });
+// }
 
-// FInally method(Bonus)
-// Example : 
-let loadingSpinner = true;
-function fetchData(){
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            resolve(`User Data received`)
-        },1000)
-    })
+// fetchData()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log(`Error: ${error}`);
+//   })
+//   // AlWAYS get executed!!!!
+//   .finally(() => {
+//     loadingSpinner = false;
+//     console.log(`Loading Complete -Hide spinner!!`);
+//   });
 
+// //  9. Promise Chaining : Escaping Callback Hell!
+
+// // Example 1: Proccessing Data Step by Step
+
+// function step1() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log(`Step1 Completed!!`);
+//       resolve(`Data from step1`);
+//     }, 1000);
+//   });
+// }
+
+// function step2(PreviousData) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log(`Received ${PreviousData}`);
+//       console.log(`Step2 Completed!!`);
+//       resolve(`Data from step2`);
+//     }, 1000);
+//   });
+// }
+
+// function step3(PreviousData) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log(`Received ${PreviousData}`);
+//       console.log(`Step3 Completed!!`);
+//       resolve();
+
+//     }, 1000);
+//   });
+// }
+
+// // Clean promise Chain
+
+// step1()
+//   .then((msg) => {
+//     return step2(msg);
+//   })
+//   .then((msg) => {
+//     return step3(msg);
+//     console.log(`All Done !!!`);// Doubt , why it is not getting printed
+//   })
+//   .catch((error) => {
+//     console.log(`Error: ${error}`);
+//   });
+
+// //   Example 2: Changing Background Color
+
+// function changeColorPromise(color, delay) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = color;
+//       console.log("Changed to", color);
+//       resolve();
+//     }, delay);
+//   });
+// }
+// // Clean, flat, readable!
+// changeColorPromise("red", 1000)
+//   .then(() => {
+//     return changeColorPromise("orange", 1000);
+//   })
+//   .then(() => {
+//     return changeColorPromise("green", 1000);
+//   })
+//   .then(() => {
+//     return changeColorPromise("blue", 1000);
+//   })
+//   .then(() => {
+//     console.log("All colors complete!");
+//   })
+//   .catch((error) => {
+//     console.log("Error:", error);
+//   });
+
+// // Example 3: Verification Of Email Step by step
+
+// function registerUser(username) {
+//   return new Promise((resolve) => {
+//     (setTimeout(() => {
+//       console.log(`✅ Users registered: ${username}`);
+//       resolve(username);
+//     }),
+//       1000);
+//   });
+// }
+
+// function sendVerificationEmail(username) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log("✅ Verification email sent to users", username);
+//       resolve(username);
+//     }, 1000);
+//   });
+// }
+
+// function updateDatabase(username) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log("✅ Database updated for users: ", username);
+//       resolve(username);
+//     }, 1000);
+//   });
+// }
+
+// function sendWelcomeSMS(username) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log("✅ Welcome SMS sent to users:", username);
+//       resolve(username);
+//     }, 1000);
+//   });
+// }
+
+// // Promise Chainning
+// registerUser(`Shaker_Bhai, Sahil_Bhai, Anas_Bhai`)
+//   .then(sendVerificationEmail)
+//   .then(updateDatabase)
+//   .then(sendWelcomeSMS)
+//   .then((username) => {
+//     console.log("🎉 Registration complete to users: ", username);
+//   })
+
+//   .catch((error) => {
+//     console.log("❌ Registration failed:", error);
+//   });
+
+//   Example 4:
+function getUser(UserID) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ id: UserID, name: `Zaheer` });
+    }, 1000);
+  });
 }
+
+function getUserOrders(user) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        user: user,
+        orders: [`Order1`, `Order2`, `Order3`],
+      });
+    }, 1000);
+  });
+}
+
+function calculateTotal(data) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        user: data.user,
+        orders: data.orders,
+        total: 2500,
+      });
+    }, 1000);
+  });
+}
+
+getUser(101)
+  .then((user) => {
+    console.log(`Got User :${user.name}`);
+    return getUserOrders(user);
+  })
+  .then((data) => {
+    console.log(`Got Orders : ${data.orders}`);
+    return calculateTotal(data);
+  })
+  .then((finalData) => {
+    console.log(`Final Details ...`);
+    console.log(`user: ${finalData.user.name}`);
+    console.log(
+      `Total Oders : ${finalData.orders} and no. of orders are ${finalData.orders.length}`,
+    );
+    console.log(`Total Amount : SAR ${finalData.total}`);
+  })
+  .catch((error) => {
+    console.log(`Error: ${error}`);
+  });

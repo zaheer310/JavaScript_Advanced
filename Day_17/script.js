b3b9798647ad7b4158d85df1002931931b55888b
zaheer310 <Administrator@DC1.melikshah.com>
2026-06-25 16:59:45 +0530

    Practice in JavaScript Day_17

--- Day_17/script.js
+++ Day_17/script.js
@@ -1,3 +1,30 @@
-let jsonString = `{"name": "Alice", "age":25}`;
-let obj = JSON.parse(jsonString);
-console.log(obj);
+// // Notes Day_17
+
+// // Introduction to JSON
+
+// // Working with JSON
+
+// // JSON.parse(data)
+// let jsonString = `{"name": "Alice", "age":25}`;
+// let obj = JSON.parse(jsonString);
+// console.log(obj);
+
+// //JSON.stringify(Object)
+// let user = { name: `Bob`, age: 22 };
+// let jsonString = JSON.stringify(user);
+// console.log(jsonString);
+
+// The fetch() API
+
+let url = `https://catfact.ninja/fact`;
+fetch(url)
+  .then((response) => {
+    // console.log(response);
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data.fact);
+  })
+  .catch((error) => {
+    console.log(`Error: ${error}`);
+  });
